6acf85dc5a4894c8614cc286c02a3768161fe0ac
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-08-29 16:56:52 +0200

    .gitignore update

--- src/main/java/app/service/WeatherServiceImpl.java
+++ src/main/java/app/service/WeatherServiceImpl.java
@@ -1,6 +1,5 @@
 package app.service;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import app.net.RetrofitClient;
@@ -8,11 +7,8 @@ import app.net.WeatherApi;
 import retrofit2.Response;
 
 import java.io.IOException;
-import java.sql.Array;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
